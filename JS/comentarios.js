27bf2518ad9b6a459fb1e7da3c7446d583a9b57a
lordruah <hallanbrito@gmail.com>
2022-11-20 13:45:29 -0300

    git pull

---
+++ JS/comentarios.js
@@ -0,0 +1,5 @@
+/*Aqui tem um comentario em linha
+
+o código abaixo escreve alguma mensagem no devtools*/
+console.log('Bem vindo ao Starter')
+console.log('não iria ignorar')
---
+++ JS/script.js
@@ -0,0 +1,10 @@
+//Object
+
+const person = {
+Name: 'John',
+age: 30,
+weight: 88.6,
+isa
+
+
+}
